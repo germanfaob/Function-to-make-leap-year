7e57794fc9dfb69771454af11defd77e63c24657
German Figueroa <94080266+germanfaob@users.noreply.github.com>
2022-12-16 14:54:46 +0100

    Add files via upload

---
+++ leap-year.py
@@ -0,0 +1,14 @@
+# Un año es bisiesto si cumple dos condiciones:
+# 1. Es divisible por 4 y por 100
+# 2. Es divisible por 4, por 100 y por 400
+# La función contiene las dos condiciones anteriores dentro del "if"
+
+def bisiesto(year):
+    if year % 4 == 0 and year % 100 !=0:
+        return True
+    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        return True
+    else:
+        return False
+
+print(bisiesto(2023))
